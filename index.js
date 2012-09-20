733e5ad5a5bc4f54cdfb3f39f08c01bc05388569
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2012-09-20 11:40:29 -0400

    Serve index.html by default.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(express.static(__dirname + '/public'));
 app.use('/vendor', express.static(__dirname + '/vendor'));
 app.use('/api', api);
 
+app.get('*', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
 var port = process.env.PORT || 8000;
 app.listen(port, function () {
     console.log('Listening on ' + port);
